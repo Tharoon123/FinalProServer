7ae9918ce7f084c0f390c121daa1ee9a3c297ba0
Tharoon123 <64660264+Tharoon123@users.noreply.github.com>
2025-02-07 23:05:42 +0530

    pw change2

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const upload = multer({ storage: storage });
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'Hello',
     database: 'banking'
     
 });
